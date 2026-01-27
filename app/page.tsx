b095e1e62537d0457f26d58d9f0c9096ded63d05
satoshi toyama <satoshi.toyama@route06.co.jp>
2026-01-27 17:08:53 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 export default function Home() {
 	// `/` is treated as `/articles`
-	const { redirect } = require("next/navigation") as typeof import("next/navigation");
+	const { redirect } =
+		require("next/navigation") as typeof import("next/navigation");
 	redirect("/articles");
 }
