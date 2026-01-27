700acd84c5bf8dddcda0004a11d7d8d3ac2a0c0c
satoshi toyama <satoshi.toyama@route06.co.jp>
2026-01-27 15:24:34 +0900

    feat(db): Add authentication & user management schema
    The migration creates tables for storing user authentication information and user data. The added tables and indexes are:
    - account: stores user authentication state
    - session: stores user session data
    - user: stores user profile data
    - verification: stores verification data for email and phone
    Indexes and foreign key constraints are also added to maintain data consistency.
    
    BREAKING CHANGE:
    This migration introduces changes to the database schema and may require updates to application code that interacts with the database.

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+const databaseUrl = process.env.TURSO_DATABASE_URL;
+const authToken = process.env.TURSO_AUTH_TOKEN;
+
+export default defineConfig({
+  dialect: "turso",
+  schema: "./db/*-schema.ts",
+  out: "./db/migrations",
+  dbCredentials: { url: databaseUrl ?? "", authToken },
+});
